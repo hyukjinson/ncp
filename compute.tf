0e685270f910fe70a1eee613a9b8d50dd02940f2
hyukjinson <61811157+hyukjinson@users.noreply.github.com>
2022-02-18 11:43:31 +0900

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,25 @@
+###### Server ######
+
+resource "ncloud_login_key" "lgcns" {
+  key_name = "lgcns"
+}
+
+resource "ncloud_vpc" "test" {
+  ipv4_cidr_block = "10.0.0.0/16"
+}
+
+resource "ncloud_subnet" "test" {
+  vpc_no         = ncloud_vpc.test.vpc_no
+  subnet         = cidrsubnet(ncloud_vpc.test.ipv4_cidr_block, 8, 1)
+  zone           = "KR-2"
+  network_acl_no = ncloud_vpc.test.default_network_acl_no
+  subnet_type    = "PUBLIC"
+  usage_type     = "GEN"
+}
+
+resource "ncloud_server" "server" {
+  subnet_no                 = ncloud_subnet.test.id
+  name                      = "my-tf-server"
+  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+  login_key_name            = ncloud_login_key.loginkey.key_name
+}
