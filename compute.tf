09119a0e4bd49a70860f6c42ba39fe693730023d
hyukjinson <61811157+hyukjinson@users.noreply.github.com>
2022-02-18 11:48:43 +0900

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,25 +1,33 @@
 ###### Server ######
 
-resource "ncloud_login_key" "lgcns" {
+resource "ncloud_login_key" "loginkey" {
   key_name = "lgcns"
 }
 
-resource "ncloud_vpc" "test" {
-  ipv4_cidr_block = "10.0.0.0/16"
-}
-
-resource "ncloud_subnet" "test" {
-  vpc_no         = ncloud_vpc.test.vpc_no
-  subnet         = cidrsubnet(ncloud_vpc.test.ipv4_cidr_block, 8, 1)
-  zone           = "KR-2"
-  network_acl_no = ncloud_vpc.test.default_network_acl_no
-  subnet_type    = "PUBLIC"
-  usage_type     = "GEN"
-}
-
 resource "ncloud_server" "server" {
-  subnet_no                 = ncloud_subnet.test.id
-  name                      = "my-tf-server"
-  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
-  login_key_name            = ncloud_login_key.loginkey.key_name
+  subnet_no                 = ncloud_subnet.public-sbn-shared.id
+  name                      = "bastion-lnx-svr"
+  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"
+  login_key_name            = ncloud_login_key.lgcns.key_name
+  filter {
+    name   = "product_code"
+    values = ["SSD"]
+    regex  = true
+  }
+  filter {
+    name   = "cpu_count"
+    values = ["2"]
+  }
+  filter {
+    name   = "memory_size"
+    values = ["4GB"]
+  }
+  filter {
+    name   = "base_block_storage_size"
+    values = ["50GB"]
+  }
+  filter {
+    name   = "product_type"
+    values = ["HICPU"]
+  }
 }
