0d505e597d8db921693325737821e0a8f7fd760b
hyukjinson <61811157+hyukjinson@users.noreply.github.com>
2022-02-18 13:47:44 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,8 +12,8 @@ terraform {
 ######## Provider ########
 
 provider "ncloud" {
-  #access_key  = var.access_key
-  #secret_key  = var.secret_key
+  #access_key  = var.access_key                                                    ################ 키값 변경
+  #secret_key  = var.secret_key                                                    ################ 시크릿값 변경
   region     = "KR"
   #  site        = var.site \\ KR
   support_vpc = "true"
