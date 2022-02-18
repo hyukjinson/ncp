8f894a01336d6d56e62530a7fbd6fe0f54a6bc10
hyukjinson <61811157+hyukjinson@users.noreply.github.com>
2022-02-18 13:46:26 +0900

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -3,42 +3,42 @@
 ###### Server ######
 
 resource "ncloud_login_key" "loginkey" {
-  key_name = "lgcns"
+  key_name = "lgcns"                                                                          ################ 키네임 변경
 }
 
 resource "ncloud_server" "server" {
-  subnet_no                 = ncloud_subnet.pub-sbn-shared.id
-  name                      = "bastion-lnx-svr"
-  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"
+  subnet_no                 = ncloud_subnet.pub-sbn-shared.id                                 ################ 서브넷이름 변경
+  name                      = "bastion-lnx-svr"                                               ################ 서버네임 변경
+  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"                              ################ 서버이미지 변경
   login_key_name            = ncloud_login_key.loginkey.key_name
 }
 
 data "ncloud_server_product" "product" {
-  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"
+  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"                              ################ 서버스펙 변경
 
   filter {
     name   = "product_code"
-    values = ["SSD"]
+    values = ["SSD"]                                                                          ################ 타입변경
     regex  = true
   }
 
   filter {
     name   = "cpu_count"
-    values = ["2"]
+    values = ["2"]                                                                            ################ vCPU값 변경
   }
 
   filter {
     name   = "memory_size"
-    values = ["4GB"]
+    values = ["4GB"]                                                                          ################ Memory값 변경
   }
 
   filter {
     name   = "base_block_storage_size"
-    values = ["50GB"]
+    values = ["50GB"]                                                                          ################ 부팅디스크값 변경
   }
 
   filter {
     name   = "product_type"
-    values = ["HICPU"]
+    values = ["HICPU"]                                                                         ################ CPU타입 변경
   }
 }
