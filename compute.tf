f0ff8264ef5ebbaab5ceb04878c957f61fb121d1
hyukjinson <61811157+hyukjinson@users.noreply.github.com>
2022-02-18 12:09:38 +0900

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -9,23 +9,32 @@ resource "ncloud_server" "server" {
   name                      = "bastion-lnx-svr"
   server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"
   login_key_name            = ncloud_login_key.lgcns.key_name
+}
+
+data "ncloud_server_product" "product" {
+  server_image_product_code = "SPSW0LINUX000032"  // Search by 'CentOS 7.3 (64-bit)' image classic
+
   filter {
     name   = "product_code"
     values = ["SSD"]
     regex  = true
   }
+
   filter {
     name   = "cpu_count"
     values = ["2"]
   }
+
   filter {
     name   = "memory_size"
     values = ["4GB"]
   }
+
   filter {
     name   = "base_block_storage_size"
     values = ["50GB"]
   }
+
   filter {
     name   = "product_type"
     values = ["HICPU"]
