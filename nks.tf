a32cc138681ceae9556c9527ef3b2990c34b3755
hyukjinson <61811157+hyukjinson@users.noreply.github.com>
2022-02-18 13:51:29 +0900

    Update nks.tf

--- nks.tf
+++ nks.tf
@@ -3,25 +3,25 @@
 data "ncloud_nks_versions" "version" {
   filter {
     name = "value"
-    values = ["1.20"]
+    values = ["1.20"]                                                                           ################ NKS 버전 변경
     regex = true
   }
 }
 
 resource "ncloud_login_key" "loginkey" {
-  key_name = "lgcns"
+  key_name = "lgcns"                                                                            ################ 서버키값 변경
 }
 
-resource "ncloud_nks_cluster" "cluster" {
-  cluster_type                = "SVR.VNKS.STAND.C002.M008.NET.SSD.B050.G002"
-  k8s_version                 = data.ncloud_nks_versions.version.versions.0.value
+resource "ncloud_nks_cluster" "cluster" {                                                       
+  cluster_type                = "SVR.VNKS.STAND.C002.M008.NET.SSD.B050.G002"                    ################ 마스터서버 사이즈 변경
+  k8s_version                 = data.ncloud_nks_versions.version.versions.0.value              
   login_key_name              = ncloud_login_key.loginkey.key_name
-  name                        = "nks-cluster"
-  lb_private_subnet_no        = ncloud_subnet.lb-sbn-prd.id
+  name                        = "nks-cluster"                                                   ################ 클러스터명 변경
+  lb_private_subnet_no        = ncloud_subnet.lb-sbn-prd.id                                     ################ 클러스터의 LB서브넷 이름 변경
   kube_network_plugin         = "cilium"
   subnet_no_list              = [ ncloud_subnet.nks-sbn-prd.id ]
-  vpc_no                      = ncloud_vpc.prd-vpc.id
-  zone                        = "KR-1"
+  vpc_no                      = ncloud_vpc.prd-vpc.id                                           ################ 클러스터 VPC명 변경
+  zone                        = "KR-1"                                                          ################ 클러스터 존 변경
   log {
     audit = true
   }
@@ -31,13 +31,13 @@ resource "ncloud_nks_cluster" "cluster" {
 
 resource "ncloud_nks_node_pool" "node_pool" {
   cluster_uuid   = ncloud_nks_cluster.cluster.uuid
-  node_pool_name = "node-pool"
-  node_count     = 1
-  product_code   = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002"
-  subnet_no      = ncloud_subnet.nks-sbn-prd.id
+  node_pool_name = "node-pool"                                                                  ################ 클러스터의 노드풀 이름 변경
+  node_count     = 1                                                                            ################ 노드풀 개수 변경
+  product_code   = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002"                                 ################ 노드풀 서버 스펙 변경
+  subnet_no      = ncloud_subnet.nks-sbn-prd.id                                                 ################ 노드풀 서브넷 이름 변경
   autoscale {
     enabled = true
-    min = 1
-    max = 10
+    min = 1                                                                                     ################ 노드풀 서버 Min 개수 정의(오토스케일링)        
+    max = 10                                                                                    ################ 노드풀 서버 Max 개수 정의(오토스케일링)
   }
 }
